09373ed8a1cf37b5c0b30c153977e268b8b88da6
Evelyn Kolczynski <Evelyn Kolczynski@outlook.com>
2019-12-01 18:43:49 +0100

    exercise spring-boot-rest-consuming

---
+++ Exercises/spring-boot-rest-consuming/src/main/java/com/example/springbootrestconsuming/SpringBootRestConsumingApplication.java
@@ -0,0 +1,36 @@
+package com.example.springbootrestconsuming;
+
+import com.example.springbootrestconsuming.entity.Joke;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+
+@SpringBootApplication
+public class SpringBootRestConsumingApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(SpringBootRestConsumingApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestConsumingApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Joke joke = restTemplate.getForObject(
+					"https://official-joke-api.appspot.com/random_joke", Joke.class);
+			log.info(joke.toString());
+		};
+	}
+}
---
+++ Exercises/spring-boot-rest-consuming/src/test/java/com/example/springbootrestconsuming/SpringBootRestConsumingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootrestconsuming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestConsumingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
