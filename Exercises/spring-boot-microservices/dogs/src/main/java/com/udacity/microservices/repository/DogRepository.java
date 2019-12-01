61081d37c1375eb2d2016b97c0c2fea5d7a9868d
Evelyn Kolczynski <Evelyn Kolczynski@outlook.com>
2019-12-01 13:52:48 +0100

    changed repository extended class

--- Exercises/spring-boot-microservices/dogs/src/main/java/com/udacity/microservices/repository/DogRepository.java
+++ Exercises/spring-boot-microservices/dogs/src/main/java/com/udacity/microservices/repository/DogRepository.java
@@ -1,9 +1,10 @@
 package com.udacity.microservices.repository;
 
 import com.udacity.microservices.entity.Dog;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface DogRepository extends CrudRepository<Dog, Long> {
+public interface DogRepository extends JpaRepository<Dog, Long> {
 }
 
 
