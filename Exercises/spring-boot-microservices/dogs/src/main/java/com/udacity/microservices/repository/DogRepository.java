fd2fe9220732ed41e1f582089f43ca1614bde88f
Evelyn Kolczynski <Evelyn Kolczynski@outlook.com>
2019-12-01 13:48:24 +0100

    finalized microservice dog

---
+++ Exercises/spring-boot-microservices/dogs/src/main/java/com/udacity/microservices/repository/DogRepository.java
@@ -0,0 +1,9 @@
+package com.udacity.microservices.repository;
+
+import com.udacity.microservices.entity.Dog;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DogRepository extends CrudRepository<Dog, Long> {
+}
+
+
