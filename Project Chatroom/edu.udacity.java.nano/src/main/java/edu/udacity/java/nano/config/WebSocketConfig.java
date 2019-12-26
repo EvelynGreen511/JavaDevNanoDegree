403af5536171dcaa30a3d86c69bafbb0de129383
Evelyn Kolczynski <Evelyn Kolczynski@outlook.com>
2019-12-27 00:34:46 +0100

    [STAGED] Testing Websocket Send Message

--- Project Chatroom/edu.udacity.java.nano/src/main/java/edu/udacity/java/nano/config/WebSocketConfig.java
+++ Project Chatroom/edu.udacity.java.nano/src/main/java/edu/udacity/java/nano/config/WebSocketConfig.java
@@ -1,11 +1,17 @@
 package edu.udacity.java.nano.config;
 
+import edu.udacity.java.nano.chat.WebSocketChatServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
 public class WebSocketConfig {
+
+    @Bean
+    public WebSocketChatServer webSocketChatServerEndpoint() {
+        return new WebSocketChatServer();
+    }
 
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
