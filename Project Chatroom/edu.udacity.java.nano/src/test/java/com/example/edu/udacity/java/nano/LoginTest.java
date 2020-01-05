8da01b7132efc8146cbe4481b86183fdd2b8024c
Evelyn Kolczynski <Evelyn Kolczynski@outlook.com>
2020-01-05 18:38:07 +0100

    finalized project

--- Project Chatroom/edu.udacity.java.nano/src/test/java/com/example/edu/udacity/java/nano/LoginTest.java
+++ Project Chatroom/edu.udacity.java.nano/src/test/java/com/example/edu/udacity/java/nano/LoginTest.java
@@ -1,19 +1,14 @@
 package com.example.edu.udacity.java.nano;
 
 import edu.udacity.java.nano.WebSocketChatApplication;
-import edu.udacity.java.nano.controller.LoginController;
-import edu.udacity.java.nano.model.User;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
